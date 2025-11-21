d4612651529969a259e4f0f2947a1e07b06f6722
aldirleyg <aldirleyazevedogomes@gmail.com>
2025-11-21 11:31:15 -0100

    Instalado Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/tailwind-output.css";
 import Layout from "@/components/Layout"
+import {Analytics} from "@vercel/analytics/react";
 
 export default function App ({ Component, pageProps}) {
 
 return (
   <Layout>
     <Component {...pageProps} />
+    <Analytics />
   </Layout>
   
  );
